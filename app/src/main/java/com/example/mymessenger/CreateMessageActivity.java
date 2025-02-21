c102442199e68a4909c4ae1c12558d4e9f6f20a8
nicogame2004 <nicolas28c05@gmail.com>
2025-02-20 20:08:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,27 @@
+package com.example.mymessenger;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends Activity {
+
+    public static final String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+}
